78905438b542f5bd8a7e478cb9b2f09fc103a281
Sho Hashimoto <hashimoto@shokai.org>
2011-02-13 14:25:22 +0900

    log via websocket

---
+++ iphone/iphone-js-console.js
@@ -0,0 +1,20 @@
+var JsConsole = function(addr){
+    var ws;
+    this.start = function(addr){
+        this.ws = new WebSocket(addr);
+        this.ws.onmessage = function(e){
+            console.log(e.data);
+        };
+        this.ws.onclose = function(){
+            this.connection = false;
+            alert('js console - server closed');
+        };
+        this.ws.onopen = function(){
+            this.connection = true;
+            alert('js console - start');
+        };
+    };
+    this.log = function(message){
+        this.ws.send(message);
+    };
+};

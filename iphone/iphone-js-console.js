a7dedc874e82247d5966c2768f2b43f2bdb3a6a1
Sho Hashimoto <hashimoto@shokai.org>
2011-06-02 14:31:58 +0900

    reconnect websocket on close

--- iphone/iphone-js-console.js
+++ iphone/iphone-js-console.js
@@ -1,24 +1,33 @@
 var JsConsole = {};
 JsConsole.ws = null;
 JsConsole.connected = false;
+JsConsole.ws_connect_timer = null;
 JsConsole.start = function(addr){ // addr = "ws://192.168.1.101:8088"
-    this.ws = new WebSocket(addr);
-    this.ws.onmessage = function(e){
-        try{
-            eval("console.log("+e.data+")");
+    var connect = function(){
+        JsConsole.ws = new WebSocket(addr);
+        JsConsole.ws.onmessage = function(e){
+            try{
+                eval("console.log("+e.data+")");
+            }
+            catch(e){
+                console.log("error : "+e.message);
+            }
+        };
+        JsConsole.ws.onclose = function(){
+            if(JsConsole.connected) alert('js console - server closed');
+            JsConsole.connected = false;
+        };
+        JsConsole.ws.onopen = function(){
+            JsConsole.connected = true;
+            alert('js console - start');
+        };
+    };
+    connect();
+    this.ws_connect_timer = setInterval(function(){
+        if(JsConsole.ws == null || JsConsole.ws.readyState != 1){
+            connect();
         }
-        catch(e){
-            console.log("error : "+e.message);
-        }
-    };
-    this.ws.onclose = function(){
-        JsConsole.connected = false;
-        alert('js console - server closed');
-    };
-    this.ws.onopen = function(){
-        JsConsole.connected = true;
-        alert('js console - start');
-    };
+    }, 1000);
 };
 JsConsole.send = function(message){
     this.ws.send(message);
